dc2e9df1b5c1ec58700e89ca7980df99c077a3bd
Allyson Hotchkin <ahotchk1@yahoo.com>
2017-12-03 22:55:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ function calculatesFarePrice(start, destination){
   if (Math.abs(start-destination) < 400)
     {return 0;}
   else if (Math.abs(start-destination) > 400 && Math.abs(start-destination) <= 2000)
-    {return (Math.abs(start-destination))*0.02;}
+    {return (Math.abs(start-destination))*$0.02;}
 
 }
