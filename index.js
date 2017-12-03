2fd777685f2cf9699fa2f52bcc2471b13d21aaa6
Allyson Hotchkin <ahotchk1@yahoo.com>
2017-12-03 22:59:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,6 +16,6 @@ function calculatesFarePrice(start, destination){
     return 0;
   else if (distanceTravelledInFeet(start, destination) > 400 && distanceTravelledInFeet(start, destination) <= 2000)
     return distanceTravelledInFeet(start, destination)*0.02;
-  else if(distanceTravelledInFeet(start, destination) > 2000 && distanceTravelledInFeet(start, destination) < 2500)
+  else if (distanceTravelledInFeet(start, destination) > 2000 && distanceTravelledInFeet(start, destination) < 2500)
     return distanceTravelledInFeet(start, destination)*25;
 }
